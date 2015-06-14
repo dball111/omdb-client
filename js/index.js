971f559d9b62442de4b1adf88e9c04f3bbf9d840
Derek Ball <derekball.school@gmail.com>
2015-06-14 16:35:41 -0400

    commiting code that was already deployed on s3, now everything should be up to date. Still need to fix jeff's other comments

--- js/index.js
+++ js/index.js
@@ -1,1 +1,46 @@
-console.log('sup');
+//CreateElement and make it show on page
+
+var h1 = document.createElement("h1");
+h1.innerHTML = "OMDB Movies About Pizza";
+document.body.appendChild(h1);
+
+//make http request ie: new XHR, xhr.open 'get'
+//xhr. when you load (addEventListener)
+//xhr.send()
+
+var xhr = new XMLHttpRequest();
+xhr.open('get', 'http://www.omdbapi.com/?s=pizza');
+xhr.addEventListener('load', function (){
+
+  var response = xhr.response;
+  var responseData = JSON.parse(response);
+
+for (var i = 0; i < responseData.Search.length; i++) {
+
+  var a = document.createElement('a');
+  a.innerHTML = responseData.Search[i].Title;    //Create anchor
+  var ids = '/show.html?id=' + responseData.Search[i].imdbID + "&t=" + responseData.Search[i].Title + "&year=" + responseData.Search[i].Year + "&type=" + responseData.Search[i].Type;
+  a.href = ids;
+
+
+
+  var p = document.createElement("p");
+  p.appendChild(a);
+  document.body.appendChild(p);
+}
+});
+xhr.send(null);
+
+
+//test to try to go through url and split into objects (from ajax in playground)
+
+// var query = document.location.search;
+// var objNew = {};
+// query = query.replace("?","");
+// var x = query.split("&");
+// for (var i = 0; i < x.length; i++) {
+//   var key = x[i].split("=")[0];               //splits into key and value
+//   var value = x[i].split("=")[1];
+//   objNew[key] = value;
+//   console.log(objNew);
+// };
--- js/show.js
+++ js/show.js
@@ -1,1 +1,37 @@
-console.log('sup');
+var h1 = document.createElement('h1');
+h1.innerHTML = "Pizza Movie Ids";
+document.body.appendChild(h1);
+
+var query = document.location.search;
+var objNew = {};
+query = query.replace(/^\?/,"");
+query = query.replace(/%20/g, " ");
+query = query.replace(/%E2%80%93/g, "-");
+query = query.replace(/%96/g, "-");
+
+
+var x = query.split(/&/);
+for (var i = 0; i < x.length; i++) {
+  var key = x[i].split("=")[0];               //splits into key and value
+  var value = x[i].split("=")[1];
+  objNew[key] = value;
+  console.log(objNew);
+
+
+var xhr = new XMLHttpRequest();
+var url = 'http://www.omdbapi.com/?s=p';
+xhr.open('get', url);
+xhr.addEventListener('load', function (){
+
+  var response = xhr.response;
+  var responseData = JSON.parse(response);
+
+});
+
+};
+xhr.send(null);
+
+
+var p = document.createElement('p');
+p.innerHTML = "Movie id: " + objNew.id + "<br>" + "Movie Title: " + objNew.t + "<br>" + "Movie Year: " + objNew.year + "<br>" + "Movie Type: " + objNew.type;
+document.body.appendChild(p);
